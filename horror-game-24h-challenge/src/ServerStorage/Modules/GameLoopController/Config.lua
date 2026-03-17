9372fb96a6867c7873ed78a921b9baa96de08786
EI <darlinshot@gmail.com>
2026-03-17 21:45:33 +0700

    Add Dialog UI. Add GameLoopController to handle Game Loop.

---
+++ horror-game-24h-challenge/src/ServerStorage/Modules/GameLoopController/Config.lua
@@ -0,0 +1,5 @@
+local Config = {}
+
+Config.PLAYER_COUNT_MAX = 1
+
+return Config
---
+++ horror-game-24h-challenge/src/ServerStorage/Modules/GameLoopController/Errors.lua
@@ -0,0 +1,6 @@
+local Errors = {}
+
+Errors.NAME_NOT_NUMBER = "Module name must be a number."
+Errors.NOT_INCREMENTAL = "Module names must be incremental integer and start at 1."
+
+return Errors
